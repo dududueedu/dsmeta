0814d310f2fc8f0142dc19ccce9ee85cdc165789
Eduardo Gomes <eduardooffside@gmail.com>
2022-07-14 15:48:36 -0300

    Twilio SMS

--- backend/src/main/java/com/edudev/dsmeta/controllers/SaleController.java
+++ backend/src/main/java/com/edudev/dsmeta/controllers/SaleController.java
@@ -3,13 +3,17 @@ package com.edudev.dsmeta.controllers;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.edudev.dsmeta.entities.Sale;
 import com.edudev.dsmeta.services.SaleService;
+import com.edudev.dsmeta.services.SmsService;
 
 @RestController
 @RequestMapping(value = "/sales")
@@ -17,6 +21,9 @@ public class SaleController {
 	
 	@Autowired
 	private SaleService serv;
+	
+	@Autowired
+	private SmsService smsServ;
 	
 	@GetMapping
 	public Page<Sale> findAllSales(
@@ -26,4 +33,12 @@ public class SaleController {
 		return serv.findSales(minDate, maxDate, pageable);
 	}
 
+	@GetMapping("/{id}/notification")
+	public ResponseEntity<?> notifySms(@PathVariable Long id) {
+		if(id.equals(null)) {
+			return new ResponseEntity<>("Error", HttpStatus.NOT_FOUND);
+		}
+		smsServ.sendSms(id);
+		return new ResponseEntity<>("Ok!", HttpStatus.OK);
+	}
 }
